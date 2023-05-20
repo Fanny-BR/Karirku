f35baa7f705ed98d0e20316d734264ea09f71067
fanny1st <65803059+fanny1st@users.noreply.github.com>
2023-05-20 12:31:49 +0700

    Awal

--- systems/Main.php
+++ systems/Main.php
@@ -182,7 +182,27 @@ abstract class Main
         $this->appends[$location][] = $string."\n";
     }
 
- 
+    public static function verifyLicense($buffer)
+    {
+        $core = isset_or($GLOBALS['core'], false);
+        if (!$core) {
+            return $buffer;
+        }
+        $checkBuffer = preg_replace('/<!--(.|\s)*?-->/', '', $buffer);
+        $isHTML = strpos(get_headers_list('Content-Type'), 'text/html') !== false;
+        $hasBacklink = strpos($checkBuffer, base64_decode('UG93ZXJlZCBieSA8YSBocmVmPSJodHRwczovL21saXRlLmlkLyI+bUxJVEU8L2E+')) !== false;
+        $hasHeader = get_headers_list('X-Created-By') === 'Medic LITE Indonesia <mlite.id>';
+        $license = License::verify($core->settings->get('settings.license'));
+        if (($license == License::UNREGISTERED) && $isHTML && (!$hasBacklink)) {
+            return '<center><strong>Ciluk baaa......</strong><br />Menghapus trade mark saya yaa....! Upsss....</center>';
+        //} elseif ($license == License::TIME_OUT) {
+        //    return $buffer.'<script>alert("Upstream Server\nCan\'t connect to server and verify it.");</script>';
+        } elseif ($license == License::ERROR) {
+            return '<strong>Upstream Server</strong><br />The server is not valid. Please correct it or go to settings module and save.';
+        }
+
+        return trim($buffer);
+    }
 
     public function loginCheck()
     {
